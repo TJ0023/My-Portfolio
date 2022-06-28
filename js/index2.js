736b078b3315fffc86501bf1bc45f7f4fe11ac5f
TJ0023 <GimenezTJV@gmail.com>
2022-06-28 22:49:52 +0800

    Adjusted font-size for mobile screens

--- js/index2.js
+++ js/index2.js
@@ -3,7 +3,7 @@ function navMenu() {
     let navList = document.querySelector('.navList');
     if( navMenu.style.height === "0vh") {
     navMenu.style.height = '100vh';
-    navList.style.fontSize = '4vh';
+    navList.style.fontSize = '3vh';
   } else {
     navMenu.style.height = '0vh';
     navList.style.fontSize = '0vh';
